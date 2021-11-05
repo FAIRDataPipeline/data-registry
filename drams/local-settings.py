c933fd5c1f581bcd9e3ebaccb7f855712952c305
Nathan Cummings <nathan.cummings@ukaea.uk>
2021-11-05 09:39:03 +0000

    Add feature to export DataProduct table.

--- drams/local-settings.py
+++ drams/local-settings.py
@@ -1,5 +1,9 @@
 from .base_settings import *
 
+INSTALLED_APPS += ['import_export', ]
+
 MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware', ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+IMPORT_EXPORT_USE_TRANSACTIONS = True
