18600097464689d3ce9956c7a4220722a8523da1
Ryan Field <RyanJField@users.noreply.github.com>
2022-07-01 15:38:05 +0100

    Use loopback

--- scripts/start_fair_registry_windows.bat
+++ scripts/start_fair_registry_windows.bat
@@ -93,7 +93,8 @@ set COMMAND=['python', '%FAIR_HOME:"=%manage.py', 'runserver', '%FULL_ADDRESS%']
 @echo Spawning Server at %FULL_ADDRESS%
 if %LOG%==0 (
 	echo Disabling Logging
-	python -c "import subprocess;start_ = subprocess.Popen(%COMMAND%, stdout=subprocess.PIPE,stderr=subprocess.STDOUT,shell=False); start_.wait()"
+	echo using command %COMMAND% to spawn server
+	python -c "import subprocess;start_ = subprocess.Popen(%COMMAND%, stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=False)"
 ) else (
 	python %FAIR_HOME:"=%manage.py runserver %FULL_ADDRESS% 1> %FAIR_HOME:"=%\output.log 2>&1
 )
