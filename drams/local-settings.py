6434bd3846480f375bed21337895d953b576d778
Ryan J Field <RyanJField@users.noreply.github.com>
2022-08-15 16:15:20 +0100

    Add environment variable to allowed hosts

--- drams/local-settings.py
+++ drams/local-settings.py
@@ -1,5 +1,11 @@
 from .base_settings import *
+import os
 
 MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware', ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS.extend(
+    filter(None, os.environ.get("FAIR_ALLOWED_HOSTS", "").split(","))
+)
