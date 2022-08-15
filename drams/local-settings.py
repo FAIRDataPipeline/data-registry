73777725448c9fa59a51909a5ed1b430e6827dee
Ryan J Field <RyanJField@users.noreply.github.com>
2022-08-15 16:54:03 +0100

    remove redundant line

--- drams/local-settings.py
+++ drams/local-settings.py
@@ -4,8 +4,6 @@ import os
 MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware', ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 ALLOWED_HOSTS.extend(
     filter(None, os.environ.get("FAIR_ALLOWED_HOSTS", "").split(","))
 )
